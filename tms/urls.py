0110857e84ec17ecd3bb4f9aec29a31a72ae7c9a
Shema Leandre <iamshemaleandre@gmail.com>
2025-10-21 12:02:22 +0200

    Update contributor form with save button and list display

--- tms/urls.py
+++ tms/urls.py
@@ -1,9 +1,12 @@
 ﻿from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ContributorViewSet, TaskViewSet
+from .views import ContributorViewSet, TaskViewSet, add_contributor
 
 router = DefaultRouter()
 router.register(r'contributors', ContributorViewSet)
 router.register(r'tasks', TaskViewSet)
 
-urlpatterns = [path('', include(router.urls))]
+urlpatterns = [
+    path('', include(router.urls)),
+    path('add/', add_contributor, name='add_contributor'),
+]
--- tms/views.py
+++ tms/views.py
@@ -1,6 +1,22 @@
+from django.shortcuts import render, redirect
 from rest_framework import viewsets
 from .models import Contributor, Task
 from .serializers import ContributorSerializer, TaskSerializer
+from .forms import ContributorForm
+
+
+def add_contributor(request):
+    """View to add a new contributor."""
+    if request.method == 'POST':
+        form = ContributorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_contributor')
+    else:
+        form = ContributorForm()
+    
+    contributors = Contributor.objects.all()
+    return render(request, 'tms/add_contributor.html', {'form': form, 'contributors': contributors})
 
 
 class ContributorViewSet(viewsets.ModelViewSet):
