04a313944fe20b339feabd4e3ab8d78b9a034873
Shema Leandre <iamshemaleandre@gmail.com>
2025-10-21 11:20:47 +0200

    added form

--- tms/serializers.py
+++ tms/serializers.py
@@ -1,12 +1,12 @@
 ﻿from rest_framework import serializers
 from .models import Contributor, Task
 
+
 class ContributorSerializer(serializers.ModelSerializer):
-    full_name = serializers.ReadOnlyField()
-    
     class Meta:
         model = Contributor
         fields = '__all__'
+
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
