94d7986b34e555f5867b10e23fc5d95e4f6621c9
Shema Leandre <iamshemaleandre@gmail.com>
2025-10-22 10:58:06 +0200

    added forms url

--- task_management/settings.py
+++ task_management/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -83,12 +84,8 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tms',
-        'USER': 'postgres',
-        'PASSWORD': 'leandre',  
-        'HOST': 'localhost',
-        'PORT': '5434',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
